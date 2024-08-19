af769b339bab7c4b7a848077c6242c572ebb74f2
Jummelmarijuan <142652721+Jummelmarijuan@users.noreply.github.com>
2024-08-19 16:23:56 +0900

    updated
    added storage phonebook contacts to `localStorage'

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Filter = ({ filter, onFilterChange }) => (
+  <input
+    type="text"
+    value={filter}
+    onChange={onFilterChange}
+    placeholder="Search contacts"
+    className="block w-full border border-gray-300 rounded-md shadow-sm p-2 mt-2 mb-4 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+  />
+);
+
+export default Filter;
